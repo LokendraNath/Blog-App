3695de40d01ec3bd8484096a71384df940d018a0
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-20 12:24:41 +0530

    initilize code

---
+++ src/Layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div>
+      <header>
+        <div>
+          <div>
+          <h1>Blog App</h1>
+          <input type="text"  />
+          </div>
+          <ul>
+
+          </ul>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Layout
